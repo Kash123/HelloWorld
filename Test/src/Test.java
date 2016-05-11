f495e01a256a50f6781914f9949e9d7e64b981d0
Aakash <1994.aakashpatel@gmail.com>
2016-05-11 12:45:31 -0400

    Commit Test Project

---
+++ Test/src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+	
+	public static void loop(int num)
+	{
+		for(int i = 0; i < num; i++)
+		{
+			System.out.println("Hi, this is a test" + num);
+			
+		}
+		
+	}
+
+	public static void main (String[] args)
+	{
+		loop(5);
+		
+	}
+	
+}
